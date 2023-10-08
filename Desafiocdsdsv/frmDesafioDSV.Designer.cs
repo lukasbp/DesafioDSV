e769971052a51403d6db09ff4c2d0b6da3948b68
euae2 <darksleepo@gmail.com>
2023-10-08 02:12:44 -0300

    primeiro commit

---
+++ Desafiocdsdsv/frmDesafioDSV.Designer.cs
@@ -0,0 +1,215 @@
+﻿using ClosedXML.Excel;
+namespace Desafiocdsdsv
+{
+    partial class frmDesafioDSV
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnInsert = new Button();
+            txtDiretorio = new TextBox();
+            label1 = new Label();
+            btnBuscaEx = new Button();
+            groupBox1 = new GroupBox();
+            txtPassword = new MaskedTextBox();
+            label5 = new Label();
+            label4 = new Label();
+            txtUserid = new TextBox();
+            label3 = new Label();
+            txtDatabase = new TextBox();
+            label2 = new Label();
+            txtServer = new TextBox();
+            btnTestarCon = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(12, 126);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(212, 51);
+            btnInsert.TabIndex = 1;
+            btnInsert.Text = "Inserir no Banco";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // txtDiretorio
+            // 
+            txtDiretorio.Location = new Point(12, 97);
+            txtDiretorio.Name = "txtDiretorio";
+            txtDiretorio.Size = new Size(375, 23);
+            txtDiretorio.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Caminho da planilha";
+            // 
+            // btnBuscaEx
+            // 
+            btnBuscaEx.Location = new Point(393, 96);
+            btnBuscaEx.Name = "btnBuscaEx";
+            btnBuscaEx.Size = new Size(53, 24);
+            btnBuscaEx.TabIndex = 5;
+            btnBuscaEx.Text = "Buscar Excel";
+            btnBuscaEx.UseVisualStyleBackColor = true;
+            btnBuscaEx.Click += btnBuscaEx_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtPassword);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(txtUserid);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(txtDatabase);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(txtServer);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(434, 64);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Conexao Banco";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(324, 32);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(100, 23);
+            txtPassword.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(324, 14);
+            label5.Name = "label5";
+            label5.Size = new Size(68, 15);
+            label5.TabIndex = 7;
+            label5.Text = "PASSWORD";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(218, 14);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 15);
+            label4.TabIndex = 5;
+            label4.Text = "USER ID";
+            // 
+            // txtUserid
+            // 
+            txtUserid.Location = new Point(218, 32);
+            txtUserid.Name = "txtUserid";
+            txtUserid.Size = new Size(100, 23);
+            txtUserid.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(112, 14);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 15);
+            label3.TabIndex = 3;
+            label3.Text = "DATABASE";
+            // 
+            // txtDatabase
+            // 
+            txtDatabase.Location = new Point(112, 32);
+            txtDatabase.Name = "txtDatabase";
+            txtDatabase.Size = new Size(100, 23);
+            txtDatabase.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 1;
+            label2.Text = "SERVER";
+            // 
+            // txtServer
+            // 
+            txtServer.Location = new Point(6, 32);
+            txtServer.Name = "txtServer";
+            txtServer.Size = new Size(100, 23);
+            txtServer.TabIndex = 0;
+            // 
+            // btnTestarCon
+            // 
+            btnTestarCon.Location = new Point(234, 126);
+            btnTestarCon.Name = "btnTestarCon";
+            btnTestarCon.Size = new Size(212, 51);
+            btnTestarCon.TabIndex = 7;
+            btnTestarCon.Text = "Testar Conexão";
+            btnTestarCon.UseVisualStyleBackColor = true;
+            btnTestarCon.Click += btnTestarCon_Click;
+            // 
+            // frmDesafioDSV
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(460, 196);
+            Controls.Add(btnTestarCon);
+            Controls.Add(groupBox1);
+            Controls.Add(btnBuscaEx);
+            Controls.Add(label1);
+            Controls.Add(txtDiretorio);
+            Controls.Add(btnInsert);
+            MaximumSize = new Size(476, 235);
+            MinimumSize = new Size(476, 235);
+            Name = "frmDesafioDSV";
+            Text = "Desafio DSV";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button btnInsert;
+        private TextBox txtDiretorio;
+        private Label label1;
+        private Button btnBuscaEx;
+        private GroupBox groupBox1;
+        private Label label2;
+        private TextBox txtServer;
+        private Label label5;
+        private Label label4;
+        private TextBox txtUserid;
+        private Label label3;
+        private TextBox txtDatabase;
+        private MaskedTextBox txtPassword;
+        private Button btnTestarCon;
+    }
+}
---
+++ Desafiocdsdsv/frmDesafioDSV.cs
@@ -0,0 +1,188 @@
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Desafiocdsdsv
+{
+    public partial class frmDesafioDSV : Form
+    {
+        SqlConnection conexao;
+        public frmDesafioDSV()
+        {
+            InitializeComponent();
+
+        }
+        public string StringPreenchida()
+        {
+            validarCampos(out string erro);
+            if (!string.IsNullOrEmpty(erro))
+            {
+                MessageBox.Show(erro, "ERRO!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return "";
+            }
+            return $"Server={txtServer.Text};Database={txtDatabase.Text};User Id={txtUserid};Password={txtPassword};Trusted_Connection = True;";
+        }
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtDiretorio.Text))
+            {
+                MessageBox.Show("Arquivo não selecionado", "ERRO!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var stringConexao = StringPreenchida();
+            if (string.IsNullOrEmpty(stringConexao))
+                return;
+            try
+            {
+                conexao = new SqlConnection(stringConexao);
+                conexao.Open();
+                var xls = new XLWorkbook(txtDiretorio.Text);
+                var planilhas = xls.Worksheets.First(w => w.Name == "Cliente");
+                var totalLinhas = planilhas.Rows().Count();
+                SqlCommand cmd;
+                var ids = new List<string>();
+                for (int i = 2; i <= totalLinhas; i++)
+                {
+                    var id = planilhas.Cell($"A{i}").Value.ToString();
+                    if (!string.IsNullOrEmpty(ids.Where(a => a == id.ToString()).FirstOrDefault()))
+                    {
+                        MessageBox.Show("Cliente " + i + " não inserido pois o id já existe");
+                        continue;
+                    }
+
+                    var Nome = planilhas.Cell($"B{i}").Value.ToString();
+                    var Cidade = planilhas.Cell($"C{i}").Value.ToString();
+                    var Uf = planilhas.Cell($"D{i}").Value.ToString();
+                    var Cep = planilhas.Cell($"E{i}").Value.ToString().Replace("-", "");
+                    var Cpf = planilhas.Cell($"F{i}").Value.ToString().Replace(".", "").Replace("-", "");
+                    cmd = new SqlCommand($@"SET IDENTITY_INSERT dbo.Cliente ON;
+                                            INSERT INTO [dbo].[Cliente]
+                                                                ([ID]
+                                                                ,[Nome]
+                                                                ,[Cidade]
+                                                                ,[Uf]
+                                                                ,[Cep]
+                                                                ,[Cpf])
+                                                            VALUES
+                                                                ('{@id}',
+                                                                '{@Nome}',
+                                                                '{@Cidade}',
+                                                                '{@Uf}',
+                                                                '{@Cep}',
+                                                                '{@Cpf}');
+                                                            SET IDENTITY_INSERT dbo.Cliente OFF;", conexao);
+                    cmd.ExecuteNonQuery();
+                    ids.Add(id);
+                }
+                var planilhas2 = xls.Worksheets.First(w => w.Name == "Debitos");
+                var totalLinhas2 = planilhas2.Rows().Count();
+                for (int i = 2; i <= totalLinhas2; i++)
+                {
+                    var Fatura = planilhas2.Cell($"A{i}").Value.ToString();
+                    var Cliente = planilhas2.Cell($"B{i}").Value.ToString();
+                    if (string.IsNullOrEmpty(ids.Where(a => a == Cliente.ToString()).FirstOrDefault()))
+                    {
+                        MessageBox.Show("Débito " + i + " não inserido pois o cliente não existe");
+                        continue;
+                    }
+                    DateTime.TryParse(planilhas2.Cell($"C{i}").Value.ToString(), out DateTime Emissao);
+                    DateTime.TryParse(planilhas2.Cell($"D{i}").Value.ToString(), out DateTime Vencimento);
+                    decimal.TryParse(planilhas2.Cell($"E{i}").Value.ToString(), out decimal Valor);
+
+                    decimal.TryParse(planilhas2.Cell($"F{i}").Value.ToString(), out decimal Juros);
+                    decimal.TryParse(planilhas2.Cell($"G{i}").Value.ToString(), out decimal Descontos);
+                    var Pagamento = planilhas2.Cell($"H{i}").Value.ToString();
+                    decimal.TryParse(planilhas2.Cell($"I{i}").Value.ToString(), out decimal ValorPago);
+
+                    cmd = new SqlCommand($@"INSERT INTO [dbo].[Debitos]
+                                                                        ([Fatura]
+                                                                        ,[Cliente]
+                                                                        ,[Emissao]
+                                                                        ,[Vencimento]
+                                                                        ,[Valor]
+                                                                        ,[Juros]
+                                                                        ,[Descontos]
+                                                                        ,[Pagamento]
+                                                                        ,[ValorPago])
+                                                                    VALUES
+                                                                        ('{@Fatura}',
+                                                                        '{@Cliente}',
+                                                                        '{@Emissao}',
+                                                                        '{@Vencimento}',
+                                                                        {@Valor.ToString().Replace(',', '.')},
+                                                                        {@Juros.ToString().Replace(',', '.')},
+                                                                        {@Descontos.ToString().Replace(',', '.')},
+                                                                        {(!string.IsNullOrEmpty(Pagamento) ? $"'{DateTime.Parse(@Pagamento)}'" : "null")},
+                                                                        {@ValorPago.ToString().Replace(',', '.')})", conexao);
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Dados Inseridos!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Os dados não foram inseridos na tabela. Valide-os\n" + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void validarCampos(out string erro)
+        {
+            erro = "";
+
+            if (string.IsNullOrEmpty(txtServer.Text))
+            {
+                erro += "Campo Server vazio\n";
+            }
+            if (string.IsNullOrEmpty(txtDatabase.Text))
+            {
+                erro += "Campo Database vazio\n";
+            }
+            if (string.IsNullOrEmpty(txtUserid.Text))
+            {
+                erro += "Campo User id vazio\n";
+            }
+            if (string.IsNullOrEmpty(txtPassword.Text))
+            {
+                erro += "Campo Password vazio\n";
+            }
+
+        }
+        private void btnBuscaEx_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Planilha excel (.xlsx)|*.XLSX";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string caminhoDoExe = openFileDialog.FileName;
+                txtDiretorio.Text = caminhoDoExe;
+            }
+        }
+
+        private void btnTestarCon_Click(object sender, EventArgs e)
+        {
+            var stringConexao = StringPreenchida();
+            if (string.IsNullOrEmpty(stringConexao))
+                return;
+            var conexao = new SqlConnection(stringConexao);
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("Conexão Bem sucedida!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Conexão não estabelecida\n" + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
